5fe0598dfbb9351b2d39b58d3d74f28179f11a70
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-05-31 20:20:16 +0200

    fetches syllable mp3s as well as words. and convert them all to wav files

--- Python/SSD/prepSong.py
+++ Python/SSD/prepSong.py
@@ -2,7 +2,11 @@
 
 from syllableParser import syllableParser
 import midifile
-import urllib2, sys
+import urllib2, urllib, sys, os
+from pydub import AudioSegment
+
+MP3S_DIR = "./mp3s/"
+WAVS_DIR = MP3S_DIR.replace("mp3","wav")
 
 filename = ''
 if len(sys.argv) > 1:
@@ -51,7 +55,7 @@ print [t for (s,t) in syls[0:5]]
 
 ## check
 tracks2remove = [t for t in candidatesForRemoval if t!=noteTrack and t!=m.kartrack]
-m.write_file(filename, "__"+filename, tracks2remove, None)
+m.write_file(filename, filename.replace(".kar", "__.kar"), tracks2remove, None)
 
 
 # get TTS word files with hash to avoid copies
@@ -59,13 +63,28 @@ wordHash = {}
 for (w,t) in words:
     wordHash[w] = []
 
+## add syllables as well
+for (s,t) in syls:
+    if s != '' and s!= ' ':
+        wordHash[s.strip()] = []
+
 url = 'http://translate.google.com/translate_tts?tl=pt&q='
 header = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' }
+
+if not os.path.exists(MP3S_DIR):
+    os.makedirs(MP3S_DIR)
+if not os.path.exists(WAVS_DIR):
+    os.makedirs(WAVS_DIR)
+
 for w in wordHash:
-    response = urllib2.urlopen(urllib2.Request(url+w, None, header))
+    response = urllib2.urlopen(urllib2.Request(url+urllib.quote(w), None, header))
     responseBytes = response.read()
-    mp3FilePath = './mp3s/'+w.decode('iso-8859-1')+'.mp3'
+    mp3FilePath = MP3S_DIR+w.decode('iso-8859-1')+'.mp3'
+    wavFilePath = mp3FilePath.replace('mp3','wav')
     f = open(mp3FilePath, 'wb')
     f.write(responseBytes)
     f.close()
+    song = AudioSegment.from_mp3(mp3FilePath)
+    song.export(wavFilePath, format="wav")
+    os.remove(mp3FilePath)
 
