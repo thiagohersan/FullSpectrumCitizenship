32df496235961b051b3ba5d0c0c0b884dcbddfda
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-06-14 17:57:19 +0200

    cleans up vocode file

--- Python/SSD/vocode.py
+++ Python/SSD/vocode.py
@@ -4,55 +4,11 @@ import wave, sys
 import numpy, numpy.fft
 import math, cmath, random
 
-
-def vocode_(vWav, freq):
-    N = vWav.getnframes()/33
-    M = vWav.getsampwidth()*vWav.getnchannels()*N
-
-    framerate = vWav.getframerate()
-    sampwidth = vWav.getsampwidth()
-    window = numpy.blackman(N*vWav.getnchannels())
-    vBytes = vWav.readframes(N)
-    oBytes = []
-    currentT = 0.0
-    iAmp = 2**14
-
-    while (len(vBytes) == M):
-        vFloats = numpy.array(wave.struct.unpack("%dh"%(len(vBytes)/sampwidth), vBytes))*window
-
-        tFloats = []
-        for i in range(len(vFloats)):
-            #y = iAmp*math.sin(2*cmath.pi*freq*currentT/framerate)
-            y = 2*iAmp*(currentT/framerate*freq-math.floor(0.5+currentT/framerate*freq))
-            y = 2*abs(y)-iAmp
-            tFloats.append(y)
-            currentT += 1.0
-        tFloats = numpy.array(tFloats)*window
-
-        # Take the full (complex) fft
-        tFftData=numpy.fft.fft(tFloats)
-        vFftData=numpy.fft.fft(vFloats)
-
-        ## cartesian to polar
-        tFftPolar = [cmath.polar(z) for z in tFftData]
-        vFftPolar = [cmath.polar(z) for z in vFftData]
-
-        ## multiply magnitudes
-        oFftPolar = [(r0*r1, p0) for ((r0,p0), (r1,p1)) in zip(tFftPolar, vFftPolar)]
-
-        ## polar to cartesian
-        oFftData = [cmath.rect(r,p) for (r,p) in oFftPolar]
-
-        ## ifft(mult-mags, tPhase)
-        oFloats = numpy.fft.ifft(oFftData)
-        oFloatsR = [numpy.round(z.real) for z in oFloats]
-
-        oAmp = max(oFloatsR)
-        oBytes += [(o/oAmp*iAmp).astype('int16') for o in oFloatsR]
-
-        vBytes = vWav.readframes(N)
-
-    return oBytes
+## Maybe useful:
+# iAmp = 2**14
+# sine wave: y = iAmp*math.sin(2*cmath.pi*freq*currentT/framerate)
+# saw wave:  y = 2*iAmp*(currentT/framerate*freq-math.floor(0.5+currentT/framerate*freq))
+# triangle:  y = 2*abs(saw)-iAmp
 
 def vocode(tWav, vWav):
     N0 = 512
@@ -69,7 +25,7 @@ def vocode(tWav, vWav):
         vFloats = numpy.array(wave.struct.unpack("%dh"%(len(vBytes)/vWav.getsampwidth()), vBytes))*window
 
         iAmp = max(max(tFloats), max(vFloats))
-        vFloatSum = sum(map(abs, vFloats))
+        #vFloatSum = sum(map(abs, vFloats))
 
         # Take the full (complex) fft
         tFftData=numpy.fft.fft(tFloats)
