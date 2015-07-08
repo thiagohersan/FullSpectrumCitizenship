12de181d150519bbd5ac52916e2d10abb5ce47c5
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-07-08 20:24:07 +0200

    uses with-as to open files

--- Python/SSD/buildCorpus.py
+++ Python/SSD/buildCorpus.py
@@ -29,41 +29,36 @@ def getSongText():
     if not os.path.exists(CORPUS_DIR):
         os.makedirs(CORPUS_DIR)
 
-    corpusFile = open(os.path.join(CORPUS_DIR,"songs.txt"), 'w')
-    allKarFiles = [ os.path.join(KARS_DIR,f) for f in os.listdir(KARS_DIR) if os.path.isfile(os.path.join(KARS_DIR,f)) and f.endswith(".kar") ]
+    with open(os.path.join(CORPUS_DIR,"songs.txt"), 'w') as corpusFile:
+        allKarFiles = [ os.path.join(KARS_DIR,f) for f in os.listdir(KARS_DIR) if os.path.isfile(os.path.join(KARS_DIR,f)) and f.endswith(".kar") ]
 
-    for f in allKarFiles:
-        print "--- %s ---"%(os.path.basename(f))
-        s = Song(f, justForTheLyrics=True)
-        corpusFile.write(s.lyrics.decode('iso-8859-1').lower().encode('utf-8')+'\n\n')
-        corpusFile.flush()
-
-    corpusFile.close()
+        for f in allKarFiles:
+            print "--- %s ---"%(os.path.basename(f))
+            s = Song(f, justForTheLyrics=True)
+            corpusFile.write(s.lyrics.decode('iso-8859-1').lower().encode('utf-8')+'\n\n')
 
 def getTweetText():
     if not os.path.exists(CORPUS_DIR):
         os.makedirs(CORPUS_DIR)
 
-    corpusFile = open(os.path.join(CORPUS_DIR,"tweets.txt"), 'w')
-    jsScript = os.path.join(JS_DIR, "buildTweetCorpus.js")
+    with open(os.path.join(CORPUS_DIR,"tweets.txt"), 'w') as corpusFile:
+        jsScript = os.path.join(JS_DIR, "buildTweetCorpus.js")
 
-    for h in HASHTAGS:
-        for (t0,t1) in PERIODS:
-            numBlanks = 0
-            for o in range(0,1000,10):
-                print "--- %s[%s] [%s %s]---"%(h, o, t0, t1)
-                out = subprocess.check_output("phantomjs %s %s %s %s %s"%(jsScript, h, o, t0, t1), shell=True)
-                print out
-                corpusFile.write(out)
-                if (len(out) < 1) or (out.isspace()):
-                    numBlanks += 1
-                else:
-                    numBlanks = 0
-                # if 5 blanks in a row ... go to next hashtag/period (this also triggers if 50 RTs in a row)
-                if numBlanks > 4:
-                    break
-
-    corpusFile.close()
+        for h in HASHTAGS:
+            for (t0,t1) in PERIODS:
+                numBlanks = 0
+                for o in range(0,1000,10):
+                    print "--- %s[%s] [%s %s]---"%(h, o, t0, t1)
+                    out = subprocess.check_output("phantomjs %s %s %s %s %s"%(jsScript, h, o, t0, t1), shell=True)
+                    print out
+                    corpusFile.write(out)
+                    if (len(out) < 1) or (out.isspace()):
+                        numBlanks += 1
+                    else:
+                        numBlanks = 0
+                    # if 5 blanks in a row ... go to next hashtag/period (this also triggers if 50 RTs in a row)
+                    if numBlanks > 4:
+                        break
 
 
 if __name__=="__main__":
