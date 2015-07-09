ff04a9aad9a4d3ad8cd143d52bc6f2d9e1bbe199
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-07-09 03:22:11 +0200

    simple script to create a portuguese pos tagger

---
+++ Python/SSD/createTagger.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import nltk
+from nltk.corpus import floresta
+import cPickle
+
+FILENAME = "txts/floresta_trigram.pos"
+
+def simplify_tag(t):
+    if '+' in t:
+        return t[t.index('+')+1:]
+    else:
+        return t
+
+tsents = floresta.tagged_sents()
+tsents = [[(w.lower(),simplify_tag(t)) for (w,t) in sent] for sent in tsents if sent]
+train = tsents[100:]
+test = tsents[:100]
+
+tagger0 = nltk.DefaultTagger('n')
+tagger1 = nltk.UnigramTagger(train, backoff=tagger0)
+tagger2 = nltk.BigramTagger(train, backoff=tagger1)
+tagger = nltk.TrigramTagger(train, backoff=tagger2)
+
+tagger.evaluate(test)
+
+
+with open(FILENAME, 'wb') as outFile:
+    cPickle.dump(tagger, outFile, -1)
+
+
+'''
+with open(FILENAME, 'rb') as inFile:
+    tagger = cPickle.load(inFile)
+tagger.tag(['o', 'gigante', 'não'.decode('utf-8'), 'é'.decode('utf-8'), 'tão'.decode('utf-8'), 'grande', 'assim'])
+'''
